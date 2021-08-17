a48d7a6c9754a89d905c31eb5e74b40862e0722f
Rafal <r.p.wojciechowski10@gmail.com>
2021-08-17 15:53:02 +0200

    rozpoczecie dodawania cssa

--- index.php
+++ index.php
@@ -12,24 +12,32 @@ if(isset($_SESSION['user']))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
 <?php
     include_once("server.php");
     ?>
-    <form action="" method="post">
+    <main>
+    <div class="login">
+        <form action="" method="post">
+        <h3> ZALOGUJ SIĘ </h3>
         <input type="text" name="name" id="" placeholder="login"><br>
         <input type="password" name="passw" id="" placeholder ="hasło"><br>
         <button type="submit" name="send">zaloguj</button>
     </form>
-
+</div>
+<div class="signup">
+    
     <form action="index.php" method="post">
-<h1> Nie masz konta? Zarajestruj się!</h1>
+<h3>ZAREJESTRUJ SIĘ</h3>
 <input type="text" name="reg_name" id="" placeholder="podaj login"><br>
 <input type="password" name="reg_pass" id="" placeholder="podaj hasło"><br>
 <button type="submit" name="reg_but">Zarejestruj</button>
 
 
     </form>
+    </div>
+</main>
 </body>
 </html>
